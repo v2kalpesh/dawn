7f6b806b4b907c2d603512d0505e758042f94057
Hasti Shah <kalpesh@hastishah.com>
2022-11-25 11:20:33 +0530

    update tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layout/*.liquid',
+  './templates/*.liquid',
+  './sections/*.liquid',
+  './snippets/*.liquid'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
